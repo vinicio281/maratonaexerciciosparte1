6248f89a3047f87a4bd221c25c85e58221d365c3
vinicio281 <vinicio15789@gmail.com>
2025-11-11 00:03:20 -0300

    Commit inicial

---
+++ ConversorMoeda/Program.cs
@@ -0,0 +1,21 @@
+﻿// Conversor de Moeda 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+      
+        Console.Write("Digite a quantia em reais (R$): ");
+        double valorReais = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a taxa de câmbio (ex.: 5.20): ");
+        double taxaCambio = Convert.ToDouble(Console.ReadLine());
+
+       
+        double valorDolares = valorReais / taxaCambio;
+
+        valorDolares = Math.Round(valorDolares, 2);
+
+        Console.WriteLine($"\nR$ {valorReais:F2} equivale a US$ {valorDolares:F2} com taxa de câmbio {taxaCambio:F2}");
+    }
+}
---
+++ ImpressãoDadosUsuário/Program.cs
@@ -0,0 +1,27 @@
+﻿//Impressão de Dados do Usuário 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Solicita os dados ao usuário
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        int idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite sua cidade: ");
+        string cidade = Console.ReadLine();
+
+       
+        string mensagemConcatenacao = "Meu nome é " + nome + ", tenho " + idade + " anos e moro em " + cidade + ".";
+        Console.WriteLine("\nUsando concatenação:");
+        Console.WriteLine(mensagemConcatenacao);
+
+      
+        string mensagemInterpolacao = $"Meu nome é {nome}, tenho {idade} anos e moro em {cidade}.";
+        Console.WriteLine("\nUsando interpolação:");
+        Console.WriteLine(mensagemInterpolacao);
+    }
+}
